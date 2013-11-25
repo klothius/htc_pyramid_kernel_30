b70112a4857fb8b5366c56f5c0b7b319a5f2d861
klothius <klothius@aol.com>
2013-11-25 22:01:18 +0100

    allow lowmem to be non contiguous

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1132,6 +1132,104 @@ void *vm_map_ram(struct page **pages, unsigned int count, int node, pgprot_t pro
 EXPORT_SYMBOL(vm_map_ram);
 
 /**
+
++ * vm_area_check_early - check if vmap area is already mapped
+
++ * @vm: vm_struct to be checked
+
++ *
+
++ * This function is used to check if the vmap area has been
+
++ * mapped already. @vm->addr, @vm->size and @vm->flags should
+
++ * contain proper values.
+
++ *
+
++ */
+
+int __init vm_area_check_early(struct vm_struct *vm)
+
+{
+
+  struct vm_struct *tmp, **p;
+
+ 
+
+  BUG_ON(vmap_ini+/**
+
++ * vm_area_check_early - check if vmap area is already mapped
+
++ * @vm: vm_struct to be checked
+
++ *
+
++ * This function is used to check if the vmap area has been
+
++ * mapped already. @vm->addr, @vm->size and @vm->flags should
+
++ * contain proper values.
+
++ *
+
++ */
+
++int __init vm_area_check_early(struct vm_struct *vm)
+
++{
+
++  struct vm_struct *tmp, **p;
+
+ 
+
++  BUG_ON(vmap_initialized);
+
++  for (p = &vmlist; (tmp = *p) != NULL; p = &tmp->next) {
+
++    if (tmp->addr >= vm->addr) {
+
++      if (tmp->addr < vm->addr + vm->size)
+
++        return 1;
+
++    } else {
+
++      if (tmp->addr + tmp->size > vm->addr)
+
++        return 1;
+
++    }
+
++  }
+
++  return 0;
+
++}tialized);
+
+  for (p = &vmlist; (tmp = *p) != NULL; p = &tmp->next) {
+
+    if (tmp->addr >= vm->addr) {
+
+      if (tmp->addr < vm->addr + vm->size)
+
+        return 1;
+
+    } else {
+
+      if (tmp->addr + tmp->size > vm->addr)
+
+        return 1;
+
+   }
+
+  }
+
+  return 0;
+
+}
+
+/**
  * vm_area_register_early - register vmap area early during boot
  * @vm: vm_struct to register
  * @align: requested alignment
